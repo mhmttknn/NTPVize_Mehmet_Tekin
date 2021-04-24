52566aed76c008c1396565080838ec5ef6d4ce34
Mehmet Tekin <mse.tkn24@gmail.com>
2021-04-25 01:32:27 +0300

    Hava durumu linkinden verilerin alınmasını sağlıyoruz.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,49 @@ namespace NTPVize
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnHavaDurumuBilgileriniAl = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHavaDurumuBilgileriniAl
+            // 
+            this.btnHavaDurumuBilgileriniAl.Location = new System.Drawing.Point(139, 12);
+            this.btnHavaDurumuBilgileriniAl.Name = "btnHavaDurumuBilgileriniAl";
+            this.btnHavaDurumuBilgileriniAl.Size = new System.Drawing.Size(460, 46);
+            this.btnHavaDurumuBilgileriniAl.TabIndex = 0;
+            this.btnHavaDurumuBilgileriniAl.Text = "Hava Durumu Bilgilerini Al";
+            this.btnHavaDurumuBilgileriniAl.UseVisualStyleBackColor = true;
+            this.btnHavaDurumuBilgileriniAl.Click += new System.EventHandler(this.btnHavaDurumuBilgileriniAl_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 359);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnHavaDurumuBilgileriniAl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnHavaDurumuBilgileriniAl;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace NTPVize
 {
     public partial class Form1 : Form
     {
+
+        string hava_durumu_link = "https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnHavaDurumuBilgileriniAl_Click(object sender, EventArgs e)
+        {
+            XmlDocument document1 = new XmlDocument();
+            document1.Load(hava_durumu_link);
+
+
+
         }
     }
 }
