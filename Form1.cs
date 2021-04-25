53f2c9f441d880028ae601037bd8da54f77a50e5
Mehmet Tekin <mse.tkn24@gmail.com>
2021-04-25 14:50:00 +0300

    Yeni bit text dosyası oluşturuyoruz if döngüsüyle dosyayı nereye kaydedeceğimiz ekranı yapıyoruz daha sonra for döngüsüyle dataGridView deki satırlarımızı tarıyoruz.

--- Form1.cs
+++ Form1.cs
@@ -8,10 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.IO;
 
 namespace NTPVize
 {
     public partial class Form1 : Form
+
     {
 
         string hava_durumu_link = "https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml";
@@ -19,9 +21,13 @@ namespace NTPVize
         {
             InitializeComponent();
         }
-
+  
         private void btnHavaDurumuBilgileriniAl_Click(object sender, EventArgs e)
+
+            
         {
+
+
             XmlDocument document1 = new XmlDocument();
             document1.Load(hava_durumu_link);
             XmlElement root = document1.DocumentElement;
@@ -43,7 +49,29 @@ namespace NTPVize
 
             }
 
+            Cursor.Current = Cursors.WaitCursor;
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "txt dosyası|0.txt";
+            sf.FileName = "Veriler.txt";
 
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(sf.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                fs.Close();
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    string İller = dataGridView1.Rows[i].Cells["İller"].Value.ToString();
+                    string Durum = dataGridView1.Rows[i].Cells["Durum"].Value.ToString();
+                    string Max_Sicaklik = dataGridView1.Rows[i].Cells["Maksimum Sıcaklık"].Value.ToString();
+                    string Min_Sicaklik = dataGridView1.Rows[i].Cells["Minimum Sıcaklık"].Value.ToString();
+                    string icerik = İller + "\t|\t" + Durum + "\t|\t" + Max_Sicaklik + "\t|\t" + Min_Sicaklik;
+                    File.AppendAllText(sf.FileName, icerik + Environment.NewLine);
+
+                }
+            }
+
+
+            Cursor.Current = Cursors.Default;
 
         }
 
@@ -51,11 +79,14 @@ namespace NTPVize
         {
             foreach (DataGridViewRow secili_satir in dataGridView1.Rows)
             {
-                if (Convert.ToInt32(secili_satir.Cells[2].Value)>16)
+                if (Convert.ToInt32(secili_satir.Cells[2].Value)>20)
                 {
                     secili_satir.DefaultCellStyle.BackColor = Color.Red;
                 }
             }
         }
+
     }
+
+
 }
